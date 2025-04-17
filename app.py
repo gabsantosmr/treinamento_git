d0e5a2cc04902110b64650e5a0baee43191b915d
Gabriel dos Santos Mota Rodrigues <16060744664@ifb.local>
2025-04-16 21:40:35 -0300

    teste CI/CD

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import requests
 
 def ola_mundo():
     response = requests.get("https://api.github.com")
-    return f"Olá, turma! API Status: {response }
+    return f"Olá, turma! API Status 2: {response }
 if __name__ == "__main__":
     ola_mundo()
